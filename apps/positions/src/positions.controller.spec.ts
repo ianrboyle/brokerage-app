506c62a5ae99bedfacbce2925c476c2cd4cc4d91
Ian Boyle <irboyle21@gmail.com>
2024-02-25 12:31:11 -0700

    fix position controller test

--- apps/positions/src/positions.controller.spec.ts
+++ apps/positions/src/positions.controller.spec.ts
@@ -1,41 +1,56 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { PositionsController } from './positions.controller';
-// import { PositionsService } from './positions.service';
-// import { getRepositoryToken } from '@nestjs/typeorm';
-// import { Position } from '../../../libs/common/src';
-// import { PositionsRepository } from './positions.repository';
-// import { CompanyProfilesService } from '../../company-profiles/src/company-profiles.service';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PositionsController } from './positions.controller';
+import { PositionsService } from './positions.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import {
+  AUTH_SERVICE,
+  COMPANY_PROFILES_SERVICE,
+  Position,
+} from '../../../libs/common/src';
+import { ConfigService } from '@nestjs/config';
 
-// describe('PositionsController', () => {
-//   let controller: PositionsController;
+describe('PositionsController', () => {
+  let controller: PositionsController;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       imports: [
-//         ConfigModule.forRoot(), // Add necessary imports here
-//         // Import other modules as needed
-//       ],
-//       providers: [
-//         PositionsService,
-//         PositionsRepository,
-//         {
-//           provide: getRepositoryToken(Position),
-//           useValue: {
-//             create: jest.fn(),
-//             save: jest.fn(),
-//             find: jest.fn(),
-//           },
-//         },
-//         ConfigService,
-//       ],
-//       controllers: [PositionsController],
-//     }).compile();
+  beforeEach(async () => {
+    let fakePositionsService: Partial<PositionsService>;
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getRepositoryToken(Position),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            find: jest.fn(),
+          },
+        },
+        {
+          provide: COMPANY_PROFILES_SERVICE,
+          useValue: {
+            emit: jest.fn(),
+            send: jest.fn(),
+          },
+        },
+        {
+          provide: AUTH_SERVICE,
+          useValue: {
+            emit: jest.fn(),
+            send: jest.fn(),
+          },
+        },
+        {
+          provide: PositionsService,
+          useValue: fakePositionsService,
+        },
+        ConfigService,
+      ],
+      controllers: [PositionsController],
+    }).compile();
 
-//     controller = module.get<PositionsController>(PositionsController);
-//   });
+    controller = module.get<PositionsController>(PositionsController);
+  });
 
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
